cabf36df70fa2c39c5500788be5e76375c715ea7
Lucca Livino <lucca.f.livino@gmail.com>
2025-06-30 12:59:56 -0400

    feat: Adiciona a funcionalidade de aparecer o botão ao diminuir a tela

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,26 @@
 import './style.css'
+import './hamburger.css'
 
-const links = document.getElementById("#links")!
-const menu = document.querySelector<HTMLAnchorElement>("#menu")!
+const menu = document.getElementById('menu')!
+const hamburger = document.getElementById('hamburger')!
+const burgerIcon = document.getElementById('burger-icon') as HTMLImageElement;
+
+hamburger.addEventListener('click', () => {
+  menu.classList.toggle('open');
+  hamburger.classList.toggle('active');
+  if (hamburger.classList.contains('active')) {
+    burgerIcon.src = '/x.svg';
+  } else {
+    burgerIcon.src = '/burger-menu-roxo.svg';
+  }
+});
+
+window.addEventListener('resize', () => {
+  if (window.innerWidth > 768) {
+    menu.classList.remove('open');
+    hamburger.classList.remove('active');
+    burgerIcon.src = '/burger-menu-roxo.svg';
+  }
+});
 
 
